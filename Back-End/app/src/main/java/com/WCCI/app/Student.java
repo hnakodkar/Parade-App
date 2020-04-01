c5325048671d73291ee8a00426455c21727a4ba4
abdul jama <jamaabdul23@gmail.com>
2020-04-01 11:57:10 -0400

    completed student and teacher pojo

--- Back-End/app/src/main/java/com/WCCI/app/Student.java
+++ Back-End/app/src/main/java/com/WCCI/app/Student.java
@@ -7,6 +7,7 @@ import javax.persistence.ManyToMany;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 
 @Entity
 public class Student {
@@ -29,4 +30,27 @@ public class Student {
         return name;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public Collection<Course> getCourses() {
+        return courses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) &&
+                Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
+
+
