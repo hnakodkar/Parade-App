a4b9a6523f8e144a034f644f8674f2041f07b3a3
Olivia Smith <Olivia.Smith.31196@gmail.com>
2020-04-15 11:39:54 -0400

    working on getting messages in order

--- Front-End/js/translateComponent.js
+++ Front-End/js/translateComponent.js
@@ -7,7 +7,7 @@ const translateMessage = (baseLanguage, translatedLanguage, message) => {
     const translationEndpoint = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 
     const apiKey = 'trnsl.1.1.20200331T021614Z.c694db314be5bc51.2c0c229d52f7f79b60f801b9a658e149e1b57ce7';
-const splitMessage = message.split('--');
+    const splitMessage = message.split('--');
 
     fetch(`${translationEndpoint}?key=${apiKey}&text=${splitMessage[1]}&lang=${baseLanguage}-${translatedLanguage}`, {
             method: "POST",
@@ -15,9 +15,8 @@ const splitMessage = message.split('--');
                  "Content-Type": "application/x-www-form-urlencoded"
              }*/
 
-        }).then(response => response.json())
-        .then(console.log(`${translationEndpoint}?key=${apiKey}&text=${message}&
-        lang=${baseLanguage}-${translatedLanguage}`))
+        })
+        .then(response => response.json())
         .then(response => displayTranslatedMessages(splitMessage[0] + response.text));
 }
 
